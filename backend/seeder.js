acce198e217b82d0bf152c2c3e4ed9e23b2baf23
Vincent <nguyenhuuvui89@gmail.com>
2024-02-21 13:41:49 -0600

    data models

---
+++ backend/seeder.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './productData/userData.js';
+import products from './productData/products.js';
+import User from './models/userModels.js';
+import Product from './models/productsModel.js';
+import Order from './models/orderModels.js';
+import connectDB from './config/db.js';
+
+
+dotenv.config();
+
+connectDB(); // Connect to MongoDB database online
+
+const importData = async () => {
+  try {
+    await Order.deleteMany(); // Delete multiple records
+    await Product.deleteMany(); // Delete multiple records
+    await User.deleteMany(); // Delete multiple records
+
+    const insertUSers = await User.insertMany(users);
+    const adminUser = insertUSers[0]._id; // get Admin data
+
+    const sampleProducts = products.map((product) => ({ ...product, user: adminUser }));
+    await Product.insertMany(sampleProducts);
+    console.log('Data Imported! '.blue.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit();
+  }
+};
+const deleteData = async () => {
+  try {
+    await Order.deleteMany(); // Delete multiple records
+    await Product.deleteMany(); // Delete multiple records
+    await User.deleteMany(); // Delete multiple records
+    console.log('Data Deleted! '.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+// Condition to choose to run which function above.
+// (Then go to package.json to add to the "script"
+// "scripts": {
+//   "start": "node backend/server.js",
+//   "server": "nodemon backend/server.js",
+//   "client": "npm start --prefix frontend",
+//   "dev": "concurrently \"npm run server\" \"npm run client\"",
+//   "data:import": "node backend/seeder.js",
+//   "data:delete": "node backend/seeder.js -d"
+// }
+// then we can run in terminal: npm run data:import or data:delete)
+
+if (process.argv[2] === '-d') {
+  deleteData();
+} else {
+  importData();
+}
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import express from 'express';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import products from './productData/products.js';
 
+connectDB(); // Connect to MongoDB database online
 const port = process.env.PORT || 6000;
 
 const app = express();
@@ -18,6 +20,7 @@ app.get('/api/products', (req, res) => {
 
 app.get('/api/products/:id', (req, res) => {
   const { id } = req.params;
+  // eslint-disable-next-line no-underscore-dangle
   const product = products.find((p) => p._id === id);
   res.json(product);
 });
